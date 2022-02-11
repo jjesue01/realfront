b84a4db0389db04b946c7319b23117828a14decd
grandyzzz <twieank@gmail.com>
2022-02-11 19:45:13 +0200

    fixed decimals issue for usdc

--- services/contract/index.js
+++ services/contract/index.js
@@ -15,7 +15,7 @@ const HOMEJAB_ADDRESS_POLYGON = getConfig().POLYGON_CONTRACT_ADDRESS
 const TOKEN_ADDRESS_POLYGON = getConfig().POLYGON_TOKEN_ADDRESS
 
 async function initContracts() {
-  const weiGasPrice = await window.web3App.eth.getGasPrice()
+  //const weiGasPrice = await window.web3App.eth.getGasPrice()
 
   const HOMEJAB_BSC = new window.web3App.eth.Contract(
     abi,
@@ -26,16 +26,16 @@ async function initContracts() {
 
   const HOMEJAB_POLYGON = new window.web3App.eth.Contract(
     abi,
-    HOMEJAB_ADDRESS_POLYGON,
-    {
-      gasPrice: weiGasPrice
-    });
+    HOMEJAB_ADDRESS_POLYGON);
   const TOKEN_POLYGON = new window.web3App.eth.Contract(
     busd,
     TOKEN_ADDRESS_POLYGON);
 
+  const polygonWeiUnit = process.env.NEXT_PUBLIC_APP_ENV === 'prod' ? 'mwei' : 'ether'
+  //const polygonWeiUnit = 'ether'
+
   return {
-    polygon: createContract(HOMEJAB_POLYGON, TOKEN_POLYGON, HOMEJAB_ADDRESS_POLYGON),
+    polygon: createContract(HOMEJAB_POLYGON, TOKEN_POLYGON, HOMEJAB_ADDRESS_POLYGON, polygonWeiUnit),
     binance_smart_chain: createContract(HOMEJAB_BSC, TOKEN_BSC, HOMEJAB_ADDRESS_BSC)
   }
 }
