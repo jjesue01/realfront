ccc2c76342eb33be5e7700f3a4d0638b769aed37
grandyzzz <twieank@gmail.com>
2022-03-10 21:01:40 +0200

    created OrderNFT page

---
+++ pages/order-nft.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from '../styles/OrderNFT.module.sass'
+import Head from "next/head";
+
+function OrderNft() {
+  return (
+    <main className={styles.root}>
+      <Head>
+        <title>HOMEJAB - Order Custom NFT</title>
+      </Head>
+      <h1>hey</h1>
+    </main>
+  )
+}
+
+export default OrderNft
